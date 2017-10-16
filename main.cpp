c507e56cfff4b8a63bcfef56e8dce216292d4b1d
DolanL <32385280+DolanL@users.noreply.github.com>
2017-10-16 19:02:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,36 +9,36 @@ int main() {
 	int b[m][k];
 	int c[n][k];
 
-for (int i = 0; i < n; i++) {
-for (int j = 0; j < m; j++) {
-cin >> a[i][j];
-}
-}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> a[i][j];
+		}
+	}
 
-for (int i = 0; i < m; i++) {
-for (int j = 0; j < k; j++) {
-cin >> b[i][j];
-}
-}
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < k; j++) {
+			cin >> b[i][j];
+		}
+	}
 
-for (int i = 0; i < n; i++) {
-for (int j = 0; j < k; j++) {
-c[i][j] = 0;
-}
-}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < k; j++) {
+			c[i][j] = 0;
+		}
+	}
 
-for (int i = 0; i < n; i++) {
-for (int j = 0; j < k; j++) {
-for (int s = 0; s < m; s++) {
-c[i][j] += a[i][s] * b[s][j];
-}
-}
-}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < k; j++) {
+			for (int s = 0; s < m; s++) {
+				c[i][j] += a[i][s] * b[s][j];
+			}
+		}
+	}
 
-for (int i = 0; i < n; i++) {
-for (int j = 0; j < m; j++) {
-cout << c[i][j] << " ";
-}
-}
-return 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << c[i][j] << " ";
+		}
+	}
+	return 0;
 }
